8ae33dde997557b889355eba2080e17036f2e215
Szabo Nandor Attila <nandor.attila.szabo.innovitech.hu>
2020-10-19 17:37:59 +0200

    refactor(servser-side data-source): Refactored constructor parameters

--- src/server-side/server-side-data-source.ts
+++ src/server-side/server-side-data-source.ts
@@ -18,19 +18,13 @@ export class ServerSideDataSource implements DataSource<any> {
   private dataSubject = new BehaviorSubject<any[]>(this.data);
   private loadingSubject = new BehaviorSubject<boolean>(false);
 
-  private dataSourceEndpoint: any;
-  private dataHolder: string | symbol;
-  private sort: MatSort;
-
   private paginatorInitSubscription: Subscription;
   private pageSubscription: Subscription;
   private sortSubscription: Subscription;
 
   public loading$ = this.loadingSubject.asObservable();
 
-  public withDetail: boolean;
   public paginator: MatPaginator;
-  public paginationParams: PaginationParams;
 
   public get length() {
     return this.data.length;
@@ -38,31 +32,27 @@ export class ServerSideDataSource implements DataSource<any> {
 
   /**
    *
+   * @param paginationParams Pagination configuration.
+   * @param rowSelection Needed to clear selection if [clearSelectionOnPageChange]{@link ServerSideDataSource#clearSelectionOnPageChange} is set.
+   * @param withDetail Flag to extend the response for master-detail representation.
+   * @param clearSelectionOnPageChange Flag if set the selection will be cleared on paging.
    * @param dataSourceEndpoint Data query endpoint function.
    * @param dataHolder Field of the response that holds the array to be rendered.
-   * @param paginationParams Pagination configuration.
    * @param sort `matSort` of the table.
    * @param paginatorIntl Needed to factory a `mat-paginator` component.
    * @param cdRef Needed to factory a `mat-paginator` component.
-   * @param withDetail Flag to extend the response for master-detail representation.
-   * @param clearSelectionOnPageChange Flag if set the selecction will be cleared on paging.
    */
   constructor(
-    dataSourceEndpoint: QueryFunction,
-    dataHolder: string | symbol,
-    paginationParams: PaginationParams,
-    sort: MatSort,
+    public paginationParams: PaginationParams,
     public rowSelection: SelectionModel<any>,
+    public withDetail: boolean = false,
+    public clearSelectionOnPageChange: boolean = false,
+    private dataSourceEndpoint: QueryFunction,
+    private dataHolder: string | symbol,
+    private sort: MatSort,
     private paginatorIntl: MatPaginatorIntl,
     private cdRef: ChangeDetectorRef,
-    withDetail: boolean = false,
-    public clearSelectionOnPageChange: boolean = false
   ) {
-    this.dataSourceEndpoint = dataSourceEndpoint;
-    this.dataHolder = dataHolder;
-    this.paginationParams = paginationParams;
-    this.sort = sort;
-    this.withDetail = withDetail;
     this.paginator = new MatPaginator(this.paginatorIntl, this.cdRef);
   }
 
