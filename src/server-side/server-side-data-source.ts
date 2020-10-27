10cd41ab5c9a1c67bc045af6b07a238c4a355023
Nagy, Gábor <gabor.nagy.wrk@gmail.com>
2020-10-27 14:45:34 +0100

    fix(serverSideDataSource): Observable logic refactored (#24)
    Co-authored-by: Nagy Gábor <gabor.nagy@icellmobilsoft.hu>

--- src/server-side/server-side-data-source.ts
+++ src/server-side/server-side-data-source.ts
@@ -3,9 +3,9 @@ import { DataSource } from '@angular/cdk/table';
 import { ChangeDetectorRef } from '@angular/core';
 import { MatPaginator, MatPaginatorIntl } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-import { get as _get } from 'lodash-es';
+import { get as _get, isFunction as _isFunction } from 'lodash-es';
 import { BehaviorSubject, merge, Observable, of, Subscription } from 'rxjs';
-import { catchError, map, startWith, switchMap } from 'rxjs/operators';
+import { catchError, debounceTime, finalize, map, switchMap, tap } from 'rxjs/operators';
 import { PaginationParams } from '../interfaces';
 import { DataWithQueryResponseDetails } from '../interfaces/data-with-query-response-details.interface';
 import { QueryFunction } from '../interfaces/query-function';
@@ -18,10 +18,9 @@ export class ServerSideDataSource implements DataSource<any> {
   public data: any[] = [];
   private dataSubject = new BehaviorSubject<any[]>(this.data);
   private loadingSubject = new BehaviorSubject<boolean>(false);
+  private loadDataTriggerSubject = new BehaviorSubject<void>(null);
 
-  private paginatorInitSubscription: Subscription;
-  private pageSubscription: Subscription;
-  private sortSubscription: Subscription;
+  private loadDataSubscription: Subscription;
 
   public loading$ = this.loadingSubject.asObservable();
 
@@ -52,90 +51,88 @@ export class ServerSideDataSource implements DataSource<any> {
     private paginatorIntl: MatPaginatorIntl,
     private cdRef: ChangeDetectorRef,
     public withDetail: boolean = false,
-    public clearSelectionOnPageChange: boolean = false,
+    public clearSelectionOnPageChange: boolean = false
   ) {
+    if (!_isFunction(this.dataSourceEndpoint)) {
+      throw new Error('The `dataSourceEndpoint` argument should be a function!');
+    }
+
     this.paginator = new MatPaginator(this.paginatorIntl, this.cdRef);
   }
 
-  connect(collectionViewer: CollectionViewer): Observable<any[] | readonly any[]> {
+  connect(_collectionViewer: CollectionViewer): Observable<any[] | readonly any[]> {
     this.paginator.ngOnInit();
 
-    this.paginatorInitSubscription = this.paginator.initialized.subscribe(() => {
+    this.paginator.initialized.subscribe(() => {
       this.paginator.pageSize = this.paginationParams.rows;
     });
 
-    this.pageSubscription = this.paginator.page.subscribe(() => {
-      this.loadData();
-    });
-    this.sortSubscription = this.sort.sortChange.subscribe(() => {
-      this.loadData();
-    });
-
-    this.sort.sortChange.subscribe(() => {
-      this.loadData();
-    });
-
+    this.initDataLoading();
     this.loadData();
+
     return this.dataSubject.asObservable();
   }
 
-  disconnect(collectionViewer: CollectionViewer): void {
+  disconnect(_collectionViewer: CollectionViewer): void {
     this.dataSubject.complete();
     this.loadingSubject.complete();
-    this.pageSubscription.unsubscribe();
-    this.sortSubscription.unsubscribe();
-    this.paginatorInitSubscription.unsubscribe();
+    this.loadDataSubscription.unsubscribe();
   }
 
   /**
    * Loads data from specified endpoint.
    */
   public loadData() {
-    this.loadingSubject.next(true);
-    this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
+    this.loadDataTriggerSubject.next();
+  }
 
-    merge(this.sort.sortChange, this.paginator.page)
+  private initDataLoading() {
+    const sortChange$ = this.sort.sortChange.pipe(tap(() => (this.paginator.pageIndex = 0)));
+
+    this.loadDataSubscription = merge(this.loadDataTriggerSubject, sortChange$, this.paginator.page)
       .pipe(
-        startWith({}),
+        debounceTime(100),
         switchMap(() => {
           if (this.clearSelectionOnPageChange) {
             this.rowSelection.clear();
           }
 
-          switch (typeof this.dataSourceEndpoint) {
-            case 'function':
-              return this.dataSourceEndpoint(
-                { rows: this.paginator.pageSize, page: this.paginator.pageIndex + 1 } as QueryRequestDetails,
-                this.sort.active,
-                this.sort.direction
-              );
-            default:
-              throw new Error('The `dataSourceEndpoint` argument should be a function!');
-          }
-        }),
-        map((data: DataWithQueryResponseDetails) => {
-          this.loadingSubject.next(false);
-          this.paginator.length = data.paginationParams.totalRows;
-          this.paginationParams = {
-            ...this.paginationParams,
-            maxPage: Math.ceil(data.paginationParams.totalRows / this.paginationParams.rows),
-            totalRows: data.paginationParams.totalRows,
-            page: this.paginator.pageIndex + 1,
-          };
+          this.loadingSubject.next(true);
 
-          const rows = _get(data, this.dataHolder, []);
+          const request$ = this.dataSourceEndpoint(
+            { rows: this.paginator.pageSize, page: this.paginator.pageIndex + 1 } as QueryRequestDetails,
+            this.sort.active,
+            this.sort.direction
+          );
 
-          if (this.withDetail) {
-            rows.forEach((item: any) => (item.$detail = true));
+          if (!(request$ instanceof Observable)) {
+            throw new Error('The `dataSourceEndpoint` function should return an Observable!');
           }
 
-          return rows;
-        }),
-        catchError(() => {
-          this.loadingSubject.next(false);
-          this.paginator.length = 0;
+          return request$.pipe(
+            map((data: DataWithQueryResponseDetails) => {
+              this.paginator.length = data.paginationParams.totalRows;
+              this.paginationParams = {
+                ...this.paginationParams,
+                maxPage: Math.ceil(data.paginationParams.totalRows / this.paginationParams.rows),
+                totalRows: data.paginationParams.totalRows,
+                page: this.paginator.pageIndex + 1,
+              };
 
-          return of([]);
+              const rows = _get(data, this.dataHolder, []);
+
+              if (this.withDetail) {
+                rows.forEach((item: any) => (item.$detail = true));
+              }
+
+              return rows;
+            }),
+            catchError(() => {
+              this.paginator.length = 0;
+              return of([]);
+            }),
+            finalize(() => this.loadingSubject.next(false))
+          );
         })
       )
       .subscribe((data) => {
